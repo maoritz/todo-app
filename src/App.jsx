f75118750c31736287eca7c9415e30b48ba2e71f
Maor Attias <maorattias17@gmail.com>
2025-03-04 20:29:17 +0200

    Basic UI and functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import './style.css'
 
 const App = () => {
-  const [newItemText,setItem] = useState('')
+  const [newItem,setNewItem] = useState('')
   const [todos,setTodos] = useState([])
 
   function handleSubmit(e) {
@@ -10,18 +10,37 @@ const App = () => {
 
     setTodos(currentTodos => {
       return [
-        ...currentTodos,{id:crypto.randomUUID(), title: newItemText, completed:false}
+        ...currentTodos,{id:crypto.randomUUID(), title: newItem, completed:false}
       ]
+    })
+
+    setNewItem("") //Reset the text field after submit.
+  }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        {console.log(todo)}
+        if (todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
     })
   }
 
+  function deleteTodo (id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input value={newItemText} onChange={e => setItem(e.target.value)} type="text" id="item" />
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
         </div>
         <button className="btn" >Add</button>
       </form>
@@ -31,10 +50,19 @@ const App = () => {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input 
+                  type="checkbox" 
+                  checked={todo.completed} 
+                  onChange={e => toggleTodo(todo.id, e.target.checked)} 
+                />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button 
+                onClick={() => deleteTodo(todo.id)} 
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </li>
           )
         })}
